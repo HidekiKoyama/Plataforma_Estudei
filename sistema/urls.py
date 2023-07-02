d07e55dd179f4d37ef507c24ab8e985b13508ba9
Hideki <111573020+HidekiKoyama@users.noreply.github.com>
2023-07-01 22:10:45 -0300

    organizando melhor as views

--- sistema/urls.py
+++ sistema/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import *
+from .views_global import *
+from .views_admin import *
+
 urlpatterns = [
     path('', index, name='index'),
     path('cad_user/', cad_user, name='cad_user'),
     path('system_index/', login_is_valid, name='login_is_valid'),
-    path('cad_categories_curse/', cad_categories_curse, name="cad_categories_curse"),
-    path('update_cad_categories_curse/<int:id>/', update_cad_categories_curse, name="update_cad_categories_curse"),
+    path('cad_categories_curse/', cad_categories_curse, name='cad_categories_curse'),
+    path('update_cad_categories_curse/<int:id>/', update_cad_categories_curse, name='update_cad_categories_curse'),
     path('delete_categories_curse/<int:id>/', delete_categories_curse, name="delete_categories_curse"),
+    path('home_admin', home_admin, name='home_admin')
 ]
