93d23d286fb32bf51fdb4044e256cac74817e5e6
Hideki <111573020+HidekiKoyama@users.noreply.github.com>
2024-01-21 13:20:46 -0300

    Preparando ambinate, para continuacao

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -52,14 +52,11 @@ WSGI_APPLICATION = 'plataforma.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'plataforma_estudei',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
